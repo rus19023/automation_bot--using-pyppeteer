a35b0ba7d9e2f2cf9a7e72f93fa1b1dc328b3839
T5d0AfP <dvihalkina@gmail.com>
2022-03-24 15:55:18 +0300

    completed
    completed

--- testBrowser.py
+++ testBrowser.py
@@ -12,7 +12,7 @@ import pytz
 from concurrent.futures import ThreadPoolExecutor
 from azcaptchaapi import AZCaptchaApi
 
-api = AZCaptchaApi('pdnkdbkqgmflzcy64jwnxcbw2t9gzhmr')
+api = AZCaptchaApi('pdnkdbkqgmflzcy64jwnxcbw2t9gzhmr')                    
 start_time_step_one = '23:58:00'
 start_time_step_two = '00:00:01'
 number_browsers = 5
@@ -33,32 +33,73 @@ def start_step_two():
 		if current_time == start_time_step_two:
 			break
 		time.sleep(1)
-
 async def handle_step_one( page ):       
-        await page.goto( "http://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=pris&realmId=362&categoryId=591" ,  {'waitUntil': 'domcontentloaded'}  )   
+        await page.goto( "https://service2.diplo.de/rktermin/extern/choose_categoryList.do?locationCode=pris&realmId=362" ,  {'waitUntil': 'domcontentloaded'}  )   
+        print('current url 1 is ', await page.evaluate("() => window.location.href"))
+        print( 'next link ')
+        oneHref1 = await page.waitForXPath('//*[@id="content"]/div[1]/div[2]/a')
+        navPromise1 = asyncio.ensure_future( page.waitForNavigation( {"timeout": 60000, "waitUntil": "networkidle0"} ))
+        await oneHref1.click()      
+        await navPromise1        
+        print('current url 2 is ', await page.evaluate("() => window.location.href"))  
+        print( 'next link ')
+
+        oneHref2 = await page.waitForXPath('//*[@id="content"]/div[1]/h3[1]/a[2]')
+        navPromise2 = asyncio.ensure_future( page.waitForNavigation( {"timeout": 60000, "waitUntil": "networkidle0"} ))
+        await oneHref2.click()     
+        await navPromise2
+        print('current url 3 is ', await page.evaluate("() => window.location.href"))
+        print( 'next link ')
+
         while True:
             content, page = await solve_captcha( page )                        
             if  ('captchaText' not in content) and ('<div' in content):
-                return page                
-        
+                return page 
 
-async def handle_step_two( page, line ):
+async def handle_step_two( page, browser ):       
+        await page.goto( "https://service2.diplo.de/rktermin/extern/appointment_showForm.do?action%3Aappointment_refreshCaptcha=Load+another+picture&locationCode=pris&realmId=362&categoryId=591" ,  {'waitUntil': 'domcontentloaded'}  )           
+        print('current url is ', await page.evaluate("() => window.location.href"))
+        time.sleep( 3 )   
+        page = await browser.pages()     
+        return page[1]
+        
+async def handle_step_three( page, line ):
         # start_step_two()
         print(' handle two')
-        await page.goto( f"https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=pris&realmId=362&categoryId=591&dateStr={date_str}&openingPeriod")
+        link = "https://service2.diplo.de/rktermin/extern/appointment_addAppointment.do?locationCode=pris&realmId=362&categoryId=591&dateStr="+ date_str + "&openingPeriodId="+opening_period_id
+        
+        await page.goto( link, {'waitUntil': 'domcontentloaded'} )
+                
         print('current url is ', await page.evaluate("() => window.location.href"))
 
-        # await page.evaluate( f"""() => {{
-        #     document.getElementById("appointment_newAppointmentForm_fields_3_content").checked=true;
-        #     document.getElementById("fileds0contenthidden").value = "{line[7]}";
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[0]}';
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[1]}';
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[2]}';
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[3]}';
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[4]}';
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[5]}';
-        #     document.getElementById("appointment_captcha_month_captchaText").value = '{line[6]}';
-        #     }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_fields_3__content").checked=true;                        
+            }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_lastname").value = '{line[0]}';            
+            }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_firstname").value = '{line[1]}';            
+            }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_email").value = '{line[2]}';            
+            }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_emailrepeat").value = '{line[3]}';            
+            }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("fields0content").value = '{line[4]}';            
+            }}""" )            
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_fields_1__content").value = '{line[5]}';            
+            }}""" )
+        await page.evaluate( f"""() => {{
+            document.getElementById("appointment_newAppointmentForm_fields_2__content").value = '{line[6]}';            
+            }}""" )
+        while True:
+            content, page = await solve_captcha2( page )                        
+            if  ('captchaText' not in content) and ('<div' in content):
+                break                
         
         if 'appointment_thanx.do' in await page.evaluate("() => window.location.href"):            
             success.append( line )
@@ -90,6 +131,36 @@ async def handle_step_two( page, line ):
             
 
 async def solve_captcha( page ):        
+        print( " soleve captch ")
+        content = await page.evaluate('() => document.body.innerHTML')        
+        # content = await page.content()
+        print("content")
+        print( content )
+        tree = html.fromstring( content )          
+        captcha = tree.xpath( "//captcha/div/@style" )[0].split("'")[1].replace( "data:image/jpg;base64", "")
+        path = hashlib.md5( captcha.encode() ).hexdigest()
+        f = open( f'captchas/{path}.jpeg', 'wb' )
+        f.write( base64.b64decode( captcha ))
+        f.close()            
+
+        captcha_file = open( f'captchas/{path}.jpeg', 'rb')
+        captcha2 = api.solve(captcha_file)
+        print( " api soleve is")
+        print( captcha2.await_result() )			
+        captcha_file.close()
+        code = captcha2.await_result()                
+        await page.evaluate( f"""() => {{ document.getElementById("appointment_captcha_month_captchaText").value = '{code}'; }}""")
+        time.sleep( 0.1 )
+        element = await page.querySelector('#appointment_captcha_month_appointment_showMonth')
+        
+        navPromise = asyncio.ensure_future( page.waitForNavigation( {"timeout": 60000, "waitUntil": "networkidle0"} ))
+        
+        await element.click()        
+        await navPromise        
+        time.sleep( 4 )
+        content = await page.evaluate('() => document.body.innerHTML')
+        return content, page
+async def solve_captcha2( page ):        
         content = await page.evaluate('() => document.body.innerHTML')        
         tree = html.fromstring( content )          
         captcha = tree.xpath( "//captcha/div/@style" )[0].split("'")[1].replace( "data:image/jpg;base64", "")
@@ -103,9 +174,9 @@ async def solve_captcha( page ):
         print( captcha2.await_result() )			
         captcha_file.close()
         code = captcha2.await_result()                
-        await page.evaluate( f"""() => {{ document.getElementById("appointment_captcha_month_captchaText").value = '{code}'; }}""")
+        await page.evaluate( f"""() => {{ document.getElementById("appointment_newAppointmentForm_captchaText").value = '{code}'; }}""")
         time.sleep( 0.1 )
-        element = await page.querySelector('#appointment_captcha_month_appointment_showMonth')
+        element = await page.querySelector('#appointment_newAppointmentForm_appointment_addAppointment')
         
         navPromise = asyncio.ensure_future( page.waitForNavigation( {"timeout": 60000, "waitUntil": "networkidle0"} ))
         
@@ -113,7 +184,7 @@ async def solve_captcha( page ):
         time.sleep( 2 )
         await navPromise        
         content = await page.evaluate('() => document.body.innerHTML')
-        return content, page
+        return content, page        
 
 
 # global vars
@@ -127,7 +198,7 @@ f = open('period.txt')
 date_str, opening_period_id, googlePath = f.read().splitlines()
 
 f.close()
-print(date_str, opening_period_id)
+print(date_str, opening_period_id, googlePath)
 try:
 	makedirs('captchas')
 except:
@@ -141,23 +212,21 @@ except:
 data = pd.read_csv( 'diplo.de.csv', dtype=str).values.tolist()
 
 async def main( line ):
-        browser = await launch( headless=False, autoclose=False, executablePath=googlePath )        
+        browser = await launch( headless=False, autoclose=False, executablePath=googlePath )                
+        page = await browser.newPage()
         
-        page = await browser.newPage()                
-        try:
-            page = await handle_step_one( page )
-            await handle_step_two( page, line )        
-            # time.sleep(10)
-            # await browser.close()
+        try:            
+            resultPage = await handle_step_one( page )
+            page2 = await handle_step_two( resultPage, browser )
+            await handle_step_three( page2 , line )            
         except:
             await browser.close()           
 
 loop = asyncio.get_event_loop()
 loop.create_task( main(data[0]) )
-loop.create_task( main(data[1]) )
-loop.create_task( main(data[2]) )
-loop.create_task( main(data[3]) )
-
+# loop.create_task( main(data[1]) )
+# loop.create_task( main(data[2]) )
+# loop.create_task( main(data[3]) )
 
 try:
     loop.run_forever()
